4685c86e8b633c5235fb269e41543454b62fba52
shariarhossain23e <shariarhossain23@gamil.comm>
2022-02-13 16:56:06 +0600

    show display calc button

--- app.js
+++ app.js
@@ -14,3 +14,21 @@ document.getElementById("generate-pin").addEventListener("click",function(){
     showPin.value = getPin()
     
 });
+
+//  calc numbers
+document.getElementById("calc").addEventListener("click",function(e){
+    // console.log(e.target.innerText)
+    const calcDisplay = document.getElementById("calc-display");
+    if(isNaN(e.target.innerText)){
+        if(e.target.innerText == "C"){
+            document.getElementById("calc-display").value = ""
+        }
+        else if(e.target.innerText == "<"){
+            const displayInput =  document.getElementById("calc-display").value;
+            displayInput.slice(0,-1)
+        }
+    }
+    else{
+        calcDisplay.value += e.target.innerText;
+    }
+})
