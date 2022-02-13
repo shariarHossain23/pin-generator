2912e1fed96596e7eafa7f36239dec11df9165c4
shariarhossain23e <shariarhossain23@gamil.comm>
2022-02-13 16:36:45 +0600

    html and css

---
+++ app.js
@@ -0,0 +1,9 @@
+function getPin(){
+    const pin = Math.round(Math.random() * 10000)
+    const pinString
+}
+
+document.getElementById("generate-pin").addEventListener("click",function(){
+    const showPin = document.getElementById("show-pin");
+    const pin = getPin()
+})
