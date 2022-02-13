caf5e3ecc6ada280569e559516739af8365d38df
shariarhossain23e <shariarhossain23@gamil.comm>
2022-02-13 16:40:30 +0600

    pin generate

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 function getPin(){
     const pin = Math.round(Math.random() * 10000)
-    const pinString
+    const pinString = pin + "";
+    if(pinString.length == 4){
+        return pin;
+    }
+    else{
+        return getPin()
+    }
 }
 
 document.getElementById("generate-pin").addEventListener("click",function(){
     const showPin = document.getElementById("show-pin");
-    const pin = getPin()
-})
+    showPin.value = getPin()
+    
+});
