5b99ce7ce36db383a199ef9bf71af341f68e01f7
shariarhossain23e <shariarhossain23@gamil.comm>
2022-02-13 17:22:08 +0600

    matching pin with calc

--- app.js
+++ app.js
@@ -24,11 +24,32 @@ document.getElementById("calc").addEventListener("click",function(e){
             document.getElementById("calc-display").value = ""
         }
         else if(e.target.innerText == "<"){
-            const displayInput =  document.getElementById("calc-display").value;
-            displayInput.slice(0,-1)
+            const displayInput =  document.getElementById("calc-display");
+            displayInput.value = displayInput.value.slice(0,-1)
         }
     }
     else{
         calcDisplay.value += e.target.innerText;
     }
+})
+
+//  matching generate pin with calc
+document.getElementById("submit-btn").addEventListener("click",function(){
+    const pinInput = document.getElementById("show-pin").value;
+    const calcInput= document.getElementById("calc-display").value;
+    const successMessage = document.getElementById("done")
+    const errorMessage = document.getElementById("wrong-text")
+    if(calcInput == ""){
+        alert("dont match ")
+    }
+    else if(pinInput == calcInput){
+        successMessage.style.display = "block"
+        errorMessage.style.display = "none"
+    }
+    else{
+        errorMessage.style.display = "block"
+        successMessage.style.display = "none"
+    }
+    document.getElementById("show-pin").value = "";
+    document.getElementById("calc-display").value = "";
 })
